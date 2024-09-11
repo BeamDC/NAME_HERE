e2c1cb0c003e05f4b5f1485449af9d6acfd0edc8
Bin_Laptop <ethan.ramdeen64@gmail.com>
2024-09-11 14:50:14 -0400

    setup and some research

---
+++ src/compiler/binscript.rs
@@ -0,0 +1,16 @@
+/*
+ * Functionality:
+ * Read code into buffer
+ * Split code into tokens
+ * use tokens to piece together wasm
+ * output wasm to web module to run
+ *
+ * Components:
+ * Lexical analyser - Read the written code from a file, split it into usable tokens
+ * Syntax analyser - Ensure the written code tokens are used correctly
+ * Semantic analyser - Ensure the code correctly uses types, operators, functions, variables, etc.
+ * Intermediate Generation - Build a simple version of the code, that can be easily translated
+ * Optimisation - find generic ways to improve the generated code
+ * Generation - Build the optimised code into a program that can be run
+ */
+
--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,15 @@
+/*
+ * Goal:
+ * Create a programming language
+ * Compile said language targeting wasm
+ * knowledge here: https://www.geeksforgeeks.org/phases-of-a-compiler/
+ */
 
+mod compiler;
+mod web;
+mod tests;
+
+fn main() {
+    println!("Hello World");
+    return;
+}
