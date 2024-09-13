b7061068be1c77c889c24110572ad3ea31ca8caf
Bin_Laptop <ethan.ramdeen64@gmail.com>
2024-09-13 16:35:29 -0400

    testing updates, improved reading

--- src/editor/texteditor.rs
+++ src/editor/texteditor.rs
@@ -1,32 +1,43 @@
 use std::fs::File;
 use std::io::BufRead;
 
+const BUFFER_SIZE: usize = 8192;
+
 pub struct Textedit {
     pub file: String,
-    pub buffer: [char; 8192], // 8KB buffer, also happens to be the BufReader default size
+    pub buffer: [char; BUFFER_SIZE], // 8KiB buffer, also happens to be the BufReader default size, change to make bigger
     pub pointer: usize
 }
 
 impl Textedit {
     pub fn new() -> Self {
         Self {
-            file: "default.txt".to_owned(), // I don't like the way this is done, looks bad
-            buffer: ['\0'; 8192],
+            file: "default.txt".to_owned(),
+            buffer: ['\0'; BUFFER_SIZE],
             pointer: 0,
         }
     }
 
     pub fn read(&mut self) -> std::io::Result<()> {
+        // todo: if new file is selected, overwrite current buffer with nulls before reading
+
         let f = File::open(&self.file)?;
-        let reader = std::io::BufReader::new(f);
+        let reader = std::io::BufReader::with_capacity(BUFFER_SIZE,f);
 
         for line in reader.lines() {
             let line = line?;
-            for (i,c) in line.chars().enumerate() {
-                self.buffer[i] = c;
+            for (i, c) in line.chars().enumerate() {
+                if self.pointer + i < self.buffer.len() {
+                    self.buffer[self.pointer + i] = c;
+                } else {
+                    break;
+                }
             }
+            self.pointer += line.chars().count();
+            self.buffer[self.pointer] = '\n';
+            self.pointer += 1;
         }
-
+        self.pointer = 0;
         Ok(())
     }
 }
--- src/tests/editor_tests.rs
+++ src/tests/editor_tests.rs
@@ -1,20 +1,13 @@
 use crate::editor::texteditor::Textedit;
 #[test]
-fn test_textedit() {
+fn test_read() {
     let mut textedit = Textedit::new();
     textedit.file = "src/tests/sample_inputs.txt".to_owned();
     textedit.read().unwrap();
-    assert_eq!(textedit.buffer[0..=11].iter().copied().collect::<String>(), "Hello World!");
-    // assert_eq!(textedit.buffer[0], 'H');
-    // assert_eq!(textedit.buffer[1], 'e');
-    // assert_eq!(textedit.buffer[2], 'l');
-    // assert_eq!(textedit.buffer[3], 'l');
-    // assert_eq!(textedit.buffer[4], 'o');
-    // assert_eq!(textedit.buffer[5], ' ');
-    // assert_eq!(textedit.buffer[6], 'W');
-    // assert_eq!(textedit.buffer[7], 'o');
-    // assert_eq!(textedit.buffer[8], 'r');
-    // assert_eq!(textedit.buffer[9], 'l');
-    // assert_eq!(textedit.buffer[10], 'd');
-    // assert_eq!(textedit.buffer[11], '!');
+    assert_eq!(textedit.buffer
+                   .iter()
+                   .take_while(|&&c| c != '\0')
+                   .copied()
+                   .collect::<String>(),
+               "int main() {\n    return 0;\n}\n");
 }
