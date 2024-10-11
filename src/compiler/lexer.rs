8a2a60b0dfc4119f1d6b8433cdc9352aa5d38065
Beam <ethan.ramdeen64@gmail.com>
2024-10-10 23:13:00 -0400

    the bay harbour butcher

--- src/compiler/lexer.rs
+++ src/compiler/lexer.rs
@@ -1,38 +1,64 @@
 /*
  * read in the buffer contents. split them into tokens
+ * knowledge: https://mohitkarekar.com/posts/pl/lexer/
+ *
+ * References: ( stuff I don't really get atm)
+ * https://doc.rust-lang.org/std/iter/struct.Peekable.html
+ * https://doc.rust-lang.org/std/str/struct.CharIndices.html
  */
+use std::iter::Peekable;
+use std::str::CharIndices;
+
+const KEYWORDS: [&str; 8] = [
+    "return", "if", "else", "for", "while", "bool", "false", "true"
+
+];
 
 enum TokenType {
-    Operator,
-    Variable,
-    Function,
-    Type,
-    ControlFlow,
-    Comment,
-    Lparen,
-    Rparen,
-    Lsquare,
-    Rsquare,
-    Lcurly,
-    Rcurly,
-    Langled,
-    Rangled,
+    Keyword(Vec<char>),
+    Ident(Vec<char>),
+    String(Vec<char>),
+    Lparen(char),
+    Rparen(char),
+    Lsquare(char),
+    Rsquare(char),
+    Lcurly(char),
+    Rcurly(char),
+    Langled(char),
+    Rangled(char),
 }
 
-struct Token {
-    value: String,
+pub struct Token<'a> {
+    value: &'a str,
     species: TokenType, // 'type' didn't work, so I found a synonym
 }
 
-fn split_contents(src: &String) -> Vec<String> {
-    src.split_whitespace().collect()
+pub struct Lexer<'a> {
+    src: &'a str,
+    chars: Peekable<CharIndices<'a>>,
+    current: Option<(usize, char)>,
 }
 
-pub fn tokenize(contents: &String) -> Vec<Token> {
+impl Lexer {
+    pub fn new(src: &str) -> Lexer {
+        Lexer {
+            src: "",
+            chars: src.char_indices().peekable(),
+            current: None,
+        }
+    }
+
+    pub fn read_next(&self) {
+        // todo: get the next token and set it as the current.
+    }
+
+    pub fn match_next(&self){
+        // todo: get the next token and match it to its type
+    }
+}
+
+pub fn tokenize(contents: &Vec<u8>) -> Vec<Token> {
     let mut tokens: Vec<Token> = vec![];
-    let src = split_contents(&contents);
-
-
 
     tokens
 }
