b9d42dd9c439ed18fdea2b00ca526629b47954c9
Bin_Laptop <ethan.ramdeen64@gmail.com>
2024-10-16 10:08:31 -0400

    lexer progress

--- src/compiler/lexer.rs
+++ src/compiler/lexer.rs
@@ -6,18 +6,31 @@
  * https://doc.rust-lang.org/std/iter/struct.Peekable.html
  * https://doc.rust-lang.org/std/str/struct.CharIndices.html
  */
+
 use std::iter::Peekable;
 use std::str::CharIndices;
 
 const KEYWORDS: [&str; 8] = [
     "return", "if", "else", "for", "while", "bool", "false", "true"
-
 ];
 
-enum Token {
+const WHITESPACE: [char; 4] = [' ', '\t', '\n', '\r'];
+
+pub enum Token {
     Keyword(Vec<char>),
     Ident(Vec<char>),
+
+    // types
     String(Vec<char>),
+
+    // operators
+    Plus(char),
+    Minus(char),
+    Multiply(char),
+    Divide(char),
+    Exponential(char),
+
+    // parenthesis variations
     Lparen(char),
     Rparen(char),
     Lsquare(char),
@@ -26,6 +39,9 @@ enum Token {
     Rcurly(char),
     Langled(char),
     Rangled(char),
+
+    // control
+    Whitespace(char),
     EOF(char),
 }
 
@@ -35,7 +51,7 @@ pub struct Lexer<'a> {
     current: Option<(usize, char)>,
 }
 
-impl Lexer {
+impl Lexer<'_> {
     pub fn new(src: &str) -> Lexer {
         Lexer {
             src: "",
@@ -44,27 +60,41 @@ impl Lexer {
         }
     }
 
-    pub fn read_next(&mut self) {
+    pub fn get_next(&mut self) -> Token{
         self.current = self.chars.next();
-    }
-
-    pub fn match_next(&self) -> Token {
         let tok: Token;
+        // peek at next, if it's not whitespace, start reading multiline
+        // if it is whitespace, match and return.
+        // if first char is number, looking at numeric literal
+        // if it's a quote it's a string, read until next quote or eof
+        if WHITESPACE.iter().any(|&i| i == self.chars.peek()
+            .unwrap_or(&(0usize, '\0'))
+            .1) {
+
+        }
+
         match self.current.unwrap_or((0usize, '\0')) { // (usize, char) is the char and its position
+            (_,'+') => {tok = Token::Plus('+')}
+            (_,'-') => {tok = Token::Minus('-')}
+            (_,'*') => {tok = Token::Multiply('*')}
+            (_,'/') => {tok = Token::Divide('/')}
+            (_,'^') => {tok = Token::Exponential('^')}
+
             (_,'(') => {tok = Token::Lparen('(')}
             (_,')') => {tok = Token::Rparen(')')}
             (_,'[') => {tok = Token::Lsquare('[')}
             (_,']') => {tok = Token::Rsquare(']')}
             (_,'<') => {tok = Token::Langled('<')}
-            (_,'>') => {tok = Token::Langled('>')}
+            (_,'>') => {tok = Token::Rangled('>')}
+
+            (_,' ') => {tok = Token::Whitespace(' ')}
             _ => {tok = Token::EOF('\0')},
         }
         tok
     }
 }
 
-pub fn tokenize(contents: &Vec<u8>) -> Vec<Token> {
+pub fn tokenize(contents: &String) -> Vec<Token> {
     let mut tokens: Vec<Token> = vec![];
-
     tokens
 }
