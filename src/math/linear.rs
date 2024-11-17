3eba787f8a7e553d4b9aca3a271084f53afd9f4d
Rob <robert.homa2006@gmail.com>
2024-11-16 21:48:25 -0500

    Created some matrix stuff, nothing much yk... hbu?

---
+++ src/math/linear.rs
@@ -0,0 +1,38 @@
+// Linear Algebra, special thanks to Alireza Sayyidmousavi
+
+
+#[derive(Debug, Clone)]
+pub struct Matrix<T> {
+    pub width: usize,
+    pub height: usize,
+    pub data: Vec<Vec<T>>
+}
+
+
+
+impl<T> Matrix<T>
+where
+T:Default + Clone
+{
+    pub fn new(width: usize, height: usize) -> Matrix<T> {
+        let data = vec![vec![T::default(); width]; height];
+
+        Matrix {
+            width,
+            height,
+            data
+        }
+    }
+}
+
+pub fn transpose<T>(matrix: Matrix<T>) -> Matrix<T> {
+
+    if matrix.width * matrix.height == 0 {
+        matrix
+    }
+
+    else {
+        matrix
+    }
+
+}
